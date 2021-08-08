0356401e0fae9109ac5bcb81e86be24b56b407f7
niclas <bilbobeutlin15@gmx.de>
2021-08-08 22:03:18 +0200

    readme

--- main.py
+++ main.py
@@ -5,11 +5,32 @@ observed_values = numpy.loadtxt('data/observed_values.npy').astype(int)
 transition_probabilities = numpy.loadtxt('data/transition_probabilities.npy')
 emission_probabilities = numpy.loadtxt('data/emission_probabilities.npy')
 initial_distribution = numpy.loadtxt('data/initial_distribution.npy')
+backward_observed_values = numpy.flip(observed_values, axis=0)
 
-result = viterbi(observed_values,
+_ = viterbi(observed_values,
                  transition_probabilities,
                  emission_probabilities,
                  initial_distribution,
+                 "-noLog",
+                 log=False)
+
+_ = viterbi(backward_observed_values,
+                 transition_probabilities,
+                 emission_probabilities,
+                 initial_distribution,
+                 "-noLog-reversed",
+                 log=False)
+
+_ = viterbi(observed_values,
+                 transition_probabilities,
+                 emission_probabilities,
+                 initial_distribution,
+                 "log",
                  log=True)
 
-print("Viterbi-Path: " + result)
+_ = viterbi(backward_observed_values,
+                 transition_probabilities,
+                 emission_probabilities,
+                 initial_distribution,
+                 "log-reversed",
+                 log=True)
